01e2a611381f88b63300649d07fb720bec176319
grexess <grexess@googlemail.com>
2018-03-31 14:32:23 +0200

    doit

--- script/madscript.js
+++ script/madscript.js
@@ -4,24 +4,28 @@ function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+        x[i].style.display = "none";
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 3000); // Change image every 2 seconds
 }
 
 // Accordion
 function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-theme-d1";
-    } else { 
-        x.className = x.className.replace("w3-show", "");
-        x.previousElementSibling.className = 
-        x.previousElementSibling.className.replace(" w3-theme-d1", "");
+
+    var x = $("#" + id);
+    var y = $("#ic" + id);
+
+    if (x.hasClass("w3-show")) {
+        x.removeClass("w3-show").addClass("w3-hide");
+        //x.prev().removeClass("w3-theme-d1").addClass("w3-theme-d4");
+        y.removeClass("fa-caret-down").addClass("fa-caret-right");
+    } else {
+        x.removeClass("w3-hide").addClass("w3-show");
+        //x.prev().removeClass("w3-theme-d4").addClass("w3-theme-d1");
+        y.removeClass("fa-caret-right").addClass("fa-caret-down");
     }
 }
 
@@ -30,7 +34,7 @@ function openNav() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
-    } else { 
+    } else {
         x.className = x.className.replace(" w3-show", "");
     }
 }
@@ -40,18 +44,18 @@ function myMap() {
 
 
     var mapCanvas = document.getElementById("map");
-    var myLatLng = { lat: 50.751865, lng: 13.639060};
-    
+    var myLatLng = { lat: 50.751865, lng: 13.639060 };
+
     var mapOptions = {
-      center: new google.maps.LatLng(myLatLng),
-      zoom: 15,
-      disableDefaultUI: true
+        center: new google.maps.LatLng(myLatLng),
+        zoom: 15,
+        disableDefaultUI: true
     };
     var map = new google.maps.Map(mapCanvas, mapOptions);
-    
+
     var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'MadEast Hermsdorf'
+        position: myLatLng,
+        map: map,
+        title: 'MadEast Hermsdorf'
     });
-  }
+}
